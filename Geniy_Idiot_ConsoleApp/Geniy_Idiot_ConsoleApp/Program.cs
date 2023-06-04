b6668bf0c6d9f3c00921ba52b2e189a96ad61634
alexpronto <pronto83@yandex.ru>
2023-06-04 22:40:20 +0300

    программа для чтени записи удаления пользователей из файла json формат

--- Geniy_Idiot_ConsoleApp/Geniy_Idiot_ConsoleApp/Program.cs
+++ Geniy_Idiot_ConsoleApp/Geniy_Idiot_ConsoleApp/Program.cs
@@ -1,302 +1,199 @@
-﻿using System.Diagnostics;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using static System.Net.Mime.MediaTypeNames;
-
-internal class Program
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+namespace Readwriteusertofile
 {
-    
-
-    private static void Main(string[] args)
+    class Program
     {
+        static string DBFilePAth { get; set; }
 
-        while (true)
+        static void Main(string[] args)
         {
-            var countQuestions = 7;
+            string fileDBName = "users_geniyidiot_game";
+            string fileFolderPath = Path.GetTempPath();
+            DBFilePAth = fileFolderPath + fileDBName;
 
-            int[] arrayCountQuestions = GetArrayCountQuestions(countQuestions);
-
-            string[] questions = GetQuestions(countQuestions);
-
-            int[] answers = GetAnswers(countQuestions);
-
-            int countRightAnswers = 0;
-            
-            arrayCountQuestions = RandomizeNumbers(countQuestions, arrayCountQuestions);
-
-            string[] randomQuestions = RandomizeQuestions(questions, arrayCountQuestions, countQuestions);
-
-            int[] randomAnswers = RandomizeAnswers(answers, arrayCountQuestions, countQuestions);
-
-
-
-            Console.WriteLine("Напишите Ваше Имя");
-            string name = Console.ReadLine();
             
 
-            for (int i = 0; i < countQuestions; i++)
+            if (File.Exists(DBFilePAth) == false)
             {
-
-                int userAnswer = 0;
-
-
-                userAnswer = CheckUserAnswer(randomQuestions, i, userAnswer);
-                    
-               
-                int rightAnswer = randomAnswers[i];
-
-                if (userAnswer == rightAnswer)
-                {
-                    countRightAnswers++;
-                }
+                var file = File.Create(DBFilePAth);
+                file.Close();
             }
 
 
-            string finalDiagnose = CalculateDiagnose(countQuestions, countRightAnswers);
+            bool isWork = true;
 
-            //продумать расчет если будет вопросов меньше чем диагнозов (по модулю надло брать)
-
-            Console.WriteLine(name + ", Ваш диагноз - " + finalDiagnose);
-
-            string messageToUser = "Если желаете повторить - нажмите - ДА или НЕТ, если хотите выйти";
-
-
-            bool userChoise = GetUserChoise(messageToUser);
-
-            if (userChoise == false)
+            while (isWork)
             {
-                break;
+
+            string allCommands = "--------------\n0 - вывести всех\n1 - Добавить нового\n2 - удалить\n3 - выход\n--------------";
+            Console.WriteLine(allCommands);
+            string inputCommandStr = Console.ReadLine();
+
+            int inputCommand = GetIntFromString(inputCommandStr);
+
+            switch(inputCommand)
+            {
+                case 0:
+                    {
+                        var allUsers = ReadAllFromDB();
+                        if (allUsers.Count == 0) Console.WriteLine("пока никого нет");
+                        foreach (var user in allUsers) Console.WriteLine(user);
+                        break;
+
+                    }
+                case 1:
+                    {
+                        Console.WriteLine("Введите имя");
+                        string name = Console.ReadLine();
+
+                        Console.WriteLine("Введите фамилию");
+                        string surname = Console.ReadLine();
+
+                        User newUser = new User(0, name, surname);
+                        SaveToDB(newUser);
+                        Console.WriteLine("успешно");
+                        break;
+                    }
+
+                case 2:
+                    {
+                        Console.WriteLine("введите ID");
+                        string idStr = Console.ReadLine();
+                        int id = GetIntFromString(idStr);
+                        if (id == 0) Console.WriteLine("нет такого id");
+                        else
+                        {
+                            bool result = DeleteFromDB(id);
+                            if (result) Console.WriteLine("успешно");
+                            else Console.WriteLine("Ошибка");
+                        }
+
+
+                        break;
+                    }
+
+                 case 3:
+                    {
+                            isWork = false;
+                            Console.WriteLine("пока");
+                            break;
+
+                    }
+
+                 default:
+                    {
+                            Console.WriteLine("Нет такой команды");
+                            break;
+                    }
             }
         }
-    }
+
+        }
 
 
 
-
-    static int CheckUserAnswer(string[] randomQuestions, int i, int userAnswer)
-    {
-        bool checkAnswer = false;
-
-        while (checkAnswer == false)
+        static int GetIntFromString(string inputStr)
         {
-            
+            int input = 0;
+
             try
             {
-                checkAnswer = true;
-                Console.WriteLine();
-                Console.WriteLine("Вопрос # " + (i + 1));
-                Console.WriteLine(randomQuestions[i]);
-                userAnswer = Convert.ToInt32(Console.ReadLine());
-                
+                input = int.Parse(inputStr);
             }
 
-            catch
+            catch (FormatException)
             {
-                Console.WriteLine("ВВЕДИТЕ ЧИСЛО!");
-                Console.WriteLine();
-                checkAnswer = false;
-                
+                Console.WriteLine("Нет Такой команды");
             }
+            return input;
         }
-        return (userAnswer);
-    }
-        
 
-    static bool GetUserChoise(string message)
-    {
-
-        while (true)
+        static void SaveToDB(User user)
         {
-            Console.WriteLine(message);
-            string userInput = Console.ReadLine();
+            List<User> AllCurrentUsers = ReadAllFromDB();
+            int lastId = AllCurrentUsers.Count == 0 ? 0 : AllCurrentUsers.Last().Id;
+            //user.SetId(lastId + 1);
+            user.SetNewId(lastId + 1);
+            
+            AllCurrentUsers.Add(user);
+            string serializedUsers = JsonConvert.SerializeObject(AllCurrentUsers);
+            File.WriteAllText(DBFilePAth, serializedUsers);
 
-            if (userInput.ToLower() == "да")
+        }
+
+        static void SaveToDB(List<User> users)
+        {
+
+            string serializedUsers = JsonConvert.SerializeObject(users);
+            File.WriteAllText(DBFilePAth, serializedUsers);
+
+        }
+
+
+        static bool DeleteFromDB(int id)
+        {
+            List<User> allCurrentUsers = ReadAllFromDB();
+            User userForDeletion = allCurrentUsers.FirstOrDefault(u => u.Id == id);
+            bool result = false;
+
+            if (userForDeletion != null)
             {
-                return (true);
+                allCurrentUsers.Remove(userForDeletion);
+                result = true;
             }
+            SaveToDB(allCurrentUsers);
 
-            if (userInput.ToLower() == "нет")
-            {
-                return (false);
-            }
+            return result;
         }
 
-
-    }
-
-
-    static int[] GetArrayCountQuestions(int countQuestions)
-    {
-        int[] arrayCountQuestions = new int[countQuestions];
-
-
-        for (int i = 0; i < countQuestions; i++)
+        static List<User> ReadAllFromDB()
         {
 
-            arrayCountQuestions[i] = i + 1;
+            string json = File.ReadAllText(DBFilePAth);
+            List<User> currentUsers = JsonConvert.DeserializeObject<List<User>>(json);
+
+            return currentUsers ?? new List<User>(); 
 
         }
-
-
-        return (arrayCountQuestions);
-    }
-
-    static int[] RandomizeNumbers(int countQuestions, int[] arrayCountQuestions)
-    {
-        Random randomIndex = new Random();
-
-
-        for (int i = countQuestions - 1; i >= 0; i--)
-        {
-
-
-            int j = randomIndex.Next(i);
-
-            int tmpNumbers = arrayCountQuestions[j];
-            arrayCountQuestions[j] = arrayCountQuestions[i];
-            arrayCountQuestions[i] = tmpNumbers;
-
-
-
-
-        }
-        return (arrayCountQuestions);
-    }
-
-
-
-
-    static string[] GetQuestions(int countQuestions)
-    {
-        string[] questions = new string[countQuestions];
-        questions[0] = "Сколько будет два плюс два умноженное на два?";
-        questions[1] = "Бревно нужно распилить на 10 частей, Сколько нужно сделать распилов?";
-        questions[2] = "На двух руках 10 пальцев  (Сколько пальцев на 5 руках?)";
-        questions[3] = "Укол делают каждые пол часа  Сколько нужно минут для 3 уколов?";
-        questions[4] = "Пять свечей сгорело  Две потухли  Сколько свечей осталось?";
-        questions[5] = "Два умножить на два?";
-        questions[6] = "Три умножить на три?";
-
-        return (questions);
-    }
-
-    static int[] GetAnswers(int countQuestions)
-    {
-        int[] answers = new int[countQuestions];
-        answers[0] = 6;
-        answers[1] = 9;
-        answers[2] = 25;
-        answers[3] = 60;
-        answers[4] = 2;
-        answers[5] = 4;
-        answers[6] = 9;
-        return (answers);
-    }
-
-
-    
-
-    static string CalculateDiagnose( double countQuestions, int countRightAnswers)
-    {
-
-        var numbersOfDiagnoses = 6;
-
-        string[] diagnoses = new string[numbersOfDiagnoses];
-        diagnoses[0] = "Идиот";
-        diagnoses[1] = "Кретин";
-        diagnoses[2] = "Дурак";
-        diagnoses[3] = "Нормальный";
-        diagnoses[4] = "Талант";
-        diagnoses[5] = "Гений";
-
-        string finalDiagnose ="";
-
-        var scaleOfDiagnose = countQuestions / (numbersOfDiagnoses - 1);
-
-        if (countRightAnswers >= 0 && countRightAnswers < scaleOfDiagnose)
-        {
-            finalDiagnose = diagnoses[0]; 
-        }
-        else if (countRightAnswers >= scaleOfDiagnose && countRightAnswers < scaleOfDiagnose * 2)
-
-        {
-            finalDiagnose = diagnoses[1]; 
-
-        }
-        else if (countRightAnswers >= 2*scaleOfDiagnose && countRightAnswers < scaleOfDiagnose * 3)
-
-        {
-            finalDiagnose = diagnoses[2]; 
-
-        }
-        else if (countRightAnswers >= 3 * scaleOfDiagnose && countRightAnswers < scaleOfDiagnose * 4)
-
-        {
-            finalDiagnose = diagnoses[3]; 
-
-        }
-        else if (countRightAnswers >= 4 * scaleOfDiagnose && countRightAnswers < scaleOfDiagnose * 5)
-
-        {
-            finalDiagnose = diagnoses[4]; 
-
-        }
-        else if (countRightAnswers >= 5 * scaleOfDiagnose && countRightAnswers <= scaleOfDiagnose * 6)
-
-        {
-            finalDiagnose = diagnoses[5];
-
-        }
-
-
-
-        return (finalDiagnose);
 
         
 
+
+
     }
 
-
-
-    static string[] RandomizeQuestions(string[] questions, int[] arrayCountQuestions, int countQuestions)
+    class User // класс Юзер
     {
+        public string Name {get; private set;} // свойства класса 
+        public string Surname {get; private set;}
+        public int Id { get; private set; }
 
-
-        string[] randomQuestions = new string[countQuestions];
-
-        for (int i = 0; i < countQuestions; i++)
+        public User(int id, string name, string surname) // конструктор который принимает свойства
         {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            
+        }
 
+       
 
-            randomQuestions[i] = questions[arrayCountQuestions[i] - 1];
-
+        public void SetNewId(int id)
+        {
+            Id = id;
         }
 
 
-        return (randomQuestions);
-    }
-
-
-    static int[] RandomizeAnswers(int[] answers, int[] arrayCountQuestions, int countQuestions)
-    {
-
-
-        int[] randomAnswers = new int[countQuestions];
-
-        for (int i = 0; i < countQuestions; i++)
+        public override string ToString()
         {
-
-
-            randomAnswers[i] = answers[arrayCountQuestions[i] - 1];
-
+             return $"{Id} {Name} {Surname}";
         }
 
+       
 
-        return (randomAnswers);
+
     }
 
-
-}
-
-
-
+    }
